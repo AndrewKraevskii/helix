386f44a9a4045ad965d1b8972ca227707fc9c75b
Andrew Kraevskii <andrew.kraevskii@gmail.com>
2022-06-11 21:09:37 +0300

    add GetCustomRewardRedemption

--- channels_points.go
+++ channels_points.go
@@ -37,6 +37,15 @@ type GetCustomRewardsParams struct {
 	OnlyManageableRewards bool   `query:"only_manageable_rewards"`
 }
 
+type GetCustomRewardsRedemptionParams struct {
+	BroadcasterID string `query:"broadcaster_id"`
+	RewardId      string `query:"reward_id"`
+	ID            string `query:"id"`
+	Status        string `query:"status"`
+	After         string `query:"after"`
+	First         int    `query:"first"`
+}
+
 type ManyChannelCustomRewards struct {
 	ChannelCustomRewards []ChannelCustomReward `json:"data"`
 }
@@ -63,6 +72,32 @@ type ChannelCustomReward struct {
 	CooldownExpiresAt                 string                      `json:"cooldown_expires_at"`
 }
 
+type ManyCustomRewardRedemption struct {
+	CustomRewardRedemption []CustomRewardRedemption `json:"data"`
+	Pagination             *Pagination              `json:"pagination,omitempty"`
+}
+
+type CustomRewardRedemption struct {
+	BroadcasterName  string  `json:"broadcaster_name"`
+	BroadcasterLogin string  `json:"broadcaster_login"`
+	BroadcasterID    string  `json:"broadcaster_id"`
+	ID               string  `json:"id"`
+	UserLogin        *string `json:"user_login,omitempty"`
+	UserID           string  `json:"user_id"`
+	UserName         string  `json:"user_name"`
+	UserInput        string  `json:"user_input"`
+	Status           string  `json:"status"`
+	RedeemedAt       string  `json:"redeemed_at"`
+	Reward           Reward  `json:"reward"`
+}
+
+type Reward struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Prompt string `json:"prompt"`
+	Cost   int64  `json:"cost"`
+}
+
 type RewardImage struct {
 	Url1x string `json:"url_1x"`
 	Url2x string `json:"url_2x"`
@@ -87,6 +122,11 @@ type GlobalCooldownSettings struct {
 type ChannelCustomRewardResponse struct {
 	ResponseCommon
 	Data ManyChannelCustomRewards
+}
+
+type RedemptionResponse struct {
+	ResponseCommon
+	Data ManyCustomRewardRedemption
 }
 
 // Response for removing a custom reward
@@ -149,6 +189,19 @@ func (c *Client) GetCustomRewards(params *GetCustomRewardsParams) (*ChannelCusto
 	rewards := &ChannelCustomRewardResponse{}
 	resp.HydrateResponseCommon(&rewards.ResponseCommon)
 	rewards.Data.ChannelCustomRewards = resp.Data.(*ManyChannelCustomRewards).ChannelCustomRewards
+
+	return rewards, nil
+}
+
+func (c *Client) GetCustomRewardRedemption(params *GetCustomRewardsRedemptionParams) (*RedemptionResponse, error) {
+	resp, err := c.get("/channel_points/custom_rewards/redemptions", &ManyCustomRewardRedemption{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	rewards := &RedemptionResponse{}
+	resp.HydrateResponseCommon(&rewards.ResponseCommon)
+	rewards.Data.CustomRewardRedemption = resp.Data.(*ManyCustomRewardRedemption).CustomRewardRedemption
 
 	return rewards, nil
 }
